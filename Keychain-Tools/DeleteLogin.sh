197520511522d08cb1a8d6694102349cb9089a88
Allie Uhde <merkittencutie@gmail.com>
2017-08-07 14:40:52 +0100

    Fixed Delete Login Keychain Script

--- Keychain-Tools/DeleteLogin.sh
+++ Keychain-Tools/DeleteLogin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This script will delete the login keychain.
-if test -e /Users/$USER/Library/Keychains/login.keychain; then
+if test -e /Users/$USER/Library/Keychains/login.keychain-db; then
     echo "Login keychain found and will be deleted."
-    rm -f /Users/$USER/Library/Keychains/login.keychain
+    rm -f /Users/$USER/Library/Keychains/login.keychain-db
     exit 0
 else
     echo "No login keychain found."
